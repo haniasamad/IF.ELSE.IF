8f1bf06dabd5b5a564163b8bfb85b48382843bb0
Hania Samad <141482243+haniasamad@users.noreply.github.com>
2024-06-29 20:52:49 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var pop =prompt("Enter your Email");
+var password =prompt("Enter your Password");
+if(pop === "kashan@gmail.com"){
+    alert("you are Kashan")
+}
+
+else if (password === "kashan"){
+    alert("you are Admin")
+}
+else{
+    alert("You are user")
+}
